72a74891696be640324d66fd108c3cff10dc8599
Ashutosh Kumar <114405977+mrashutoshkumargautam@users.noreply.github.com>
2022-10-07 02:44:09 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+#===========================================================
+# Important Liberies 
+#===========================================================
+import os
+import csv
+import smtplib
+from cgitb import html
+from email import message
+from random import randint, choices
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+#===========================================================
+# Checking All Files and Collecting Resources form files..
+#===========================================================
+# FILE [A] Checking & Reading (SMTP.CSV) File.
+with open("smtp.csv") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for username, passwrd in reader:
+            smtp_username = username
+            smtp_password = passwrd
+#===========================================================
+# FILE [B] Checking & Reading (CONTACT.CSV) File.
+with open("contact.csv") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for name, email in reader:
+            client_name = name
+            client_email = email
+#===========================================================
+# FILE [C] Checking & Reading (Subject.csv) File.
+with open("subject.csv") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for subjects in reader:
+            subject_lines = subjects
+#===========================================================
+# All Files Reading & Checking End Here...
+#==================================================================================================
+# Here is the logic of program start.
+# Basically here we definig the work of python script.
+#==================================================================================================
+def send_mail(recipient, name_to_display, from_email, password, body, subj):
+    msg = MIMEMultipart()
+    msg.attach(MIMEText(body, 'plain'))
+    msg['Subject'] = subj
+    msg['From'] = f'{name_to_display} <{from_email}>'
+    msg['To'] = recipient
+#==================================================================================================
+# Main Def Logic Brack
+#==================================================================================================
+# Mail Program of Email Sending 
+#==========================================================================================
+    with open("smtp-settings.csv") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for domain, port in reader:
+            mailserver = smtplib.SMTP(domain, port)
+            mailserver.ehlo()
+            mailserver.starttls()
+            mailserver.ehlo()
+            mailserver.login(emailId, password)
+            mailserver.sendmail(emailId, email, msg.as_string())
+            mailserver.quit()
+#==========================================================================================
+#==================================================================================================
